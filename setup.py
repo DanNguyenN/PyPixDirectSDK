44934c12c5ba61952fe87c80d279bd0e83b549ce
Hanah <ndan0112@gmail.com>
2024-02-21 17:26:25 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PyPixDirectSDK',
-    version='0.0.1a',
+    version='0.0.2',
     packages=find_packages(),
     package_data={'PyPix': ['PyPix/*']},
 
